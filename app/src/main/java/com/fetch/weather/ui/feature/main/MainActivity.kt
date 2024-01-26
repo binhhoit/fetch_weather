aa3274bab7ebf63a683768eafad8c66ee48fdae3
binhhoit <binhhoit@gmail.com>
2024-01-26 20:25:39 +0700

    -- Implement lost network

--- app/src/main/java/com/fetch/weather/ui/feature/main/MainActivity.kt
+++ app/src/main/java/com/fetch/weather/ui/feature/main/MainActivity.kt
@@ -2,9 +2,11 @@ package com.fetch.weather.ui.feature.main
 
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.base.activity.BaseActivity
 import com.fetch.weather.R
+import timber.log.Timber
 
 class MainActivity : BaseActivity() {
 
