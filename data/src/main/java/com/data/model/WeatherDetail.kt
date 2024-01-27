f87f8dd3eb01fb21fd351179be87cd6cdf330ec7
binhhoit <binhhoit@gmail.com>
2024-01-27 14:05:47 +0700

    -- Add UITest page Login.

--- data/src/main/java/com/data/model/WeatherDetail.kt
+++ data/src/main/java/com/data/model/WeatherDetail.kt
@@ -2,14 +2,10 @@ package com.data.model
 
 import com.google.gson.annotations.SerializedName
 data class WeatherDetail (
-    @SerializedName("dt"         ) var dt         : Int?               = null,
     @SerializedName("main"       ) var main       : Main?              = Main(),
     @SerializedName("weather"    ) var weather    : List<Weather>      = listOf(),
     @SerializedName("clouds"     ) var clouds     : Clouds?            = Clouds(),
     @SerializedName("wind"       ) var wind       : Wind?              = Wind(),
-    @SerializedName("visibility" ) var visibility : Int?               = null,
-    @SerializedName("pop"        ) var pop        : Int?               = null,
-    @SerializedName("sys"        ) var sys        : Sys?               = Sys(),
     @SerializedName("dt_txt"     ) var timeLine      : String?            = null
 )
 
